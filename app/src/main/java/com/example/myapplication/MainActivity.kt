823f9b0e628099d46f7f41a35a9fa42dc8bb7cbe
marcinkolacz1976@gmail.com <marcinkolacz1976@gmail.com>
2023-09-15 08:58:24 +0200

    Przycisk z napisem

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //
+        findViewById<Button>(R.id.MojPrzycisk).setOnClickListener {
+            findViewById<Button>(R.id.MojPrzycisk).text = "Działa"
+        }
     }
 }
